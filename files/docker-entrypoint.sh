1a3ba9914f00ef1b3ed04caea21c73bb1e46e6ab
Guilleloper <abadcarton@gmail.com>
2018-06-07 10:16:46 +0200

    Añadido parametro akka.ask.timeout

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -34,33 +34,37 @@ drop_privs_cmd() {
 if [ "$1" = "help" ]; then
     echo "Usage: $(basename "$0") (jobmanager|taskmanager|local|help)"
     exit 0
+
 elif [ "$1" = "jobmanager" ]; then
-    echo "Starting Job Manager"
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s/jobmanager.heap.mb: 1024/jobmanager.heap.mb: ${JOB_MANAGER_HEAP_MB}/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s/# state.backend: filesystem/state.backend: filesystem/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s~# state.backend.fs.checkpointdir: hdfs://namenode-host:port/flink-checkpoints~state.backend.fs.checkpointdir: file://${CHECKPOINT_DIR}~g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "state.checkpoints.dir: file://${CHECKPOINT_DIR}/metadata" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "state.checkpoints.dir: ${CHECKPOINT_DIR}/metadata" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
-
+    echo "akka.ask.timeout: 120 s" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+
+    echo "Starting Job Manager"
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/jobmanager.sh" start-foreground cluster
+
 elif [ "$1" = "taskmanager" ]; then
     TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
-
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s/taskmanager.heap.mb: 1024/taskmanager.heap.mb: ${TASK_MANAGER_HEAP_MB}/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s/# state.backend: filesystem/state.backend: filesystem/g" "$FLINK_HOME/conf/flink-conf.yaml"
     sed -i -e "s~# state.backend.fs.checkpointdir: hdfs://namenode-host:port/flink-checkpoints~state.backend.fs.checkpointdir: file://${CHECKPOINT_DIR}~g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "state.checkpoints.dir: file://${CHECKPOINT_DIR}/metadata" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "state.checkpoints.dir: ${CHECKPOINT_DIR}/metadata" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "akka.ask.timeout: 120 s" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
 
     echo "Starting Task Manager"
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/taskmanager.sh" start-foreground
+
 elif [ "$1" = "local" ]; then
     echo "Starting local cluster"
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/jobmanager.sh" start-foreground local
